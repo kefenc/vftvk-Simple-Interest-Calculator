d9d5df11339a691e600401d554a34594e3401914
kefen chen <kefen.chen@wright.com.sg>
2021-11-28 13:05:14 +0800

    changes

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 function compute() {
     p = document.getElementById("principal").value;
+    if (p.length == 0) {
+        alert("Please input a positive number");
+        document.getElementById("principal").focus();
+        return
+    }
+    if (p == 0) {
+        alert("Please input a number greater than 0");
+        document.getElementById("principal").focus();
+        return
+    }
+    if (p < 0) {
+        alert("Please input a positive number");
+        document.getElementById("principal").focus();
+        return
+    }
     r = document.getElementById("rate").value;
     y = document.getElementById("years").value;
     simple = 1 + (r * y) / 100
     results = p * simple - p
     date = new Date()
     future = parseInt(date.getFullYear()) + parseInt(y)
-    document.getElementById("result").innerHTML = " If you deposit " + p + ",<br> at an interest rate of " + r + " % .<br> You will receive an amount of " + results + ",<br> in the year " + future + " "
+    document.getElementById("result").innerHTML = " If you deposit <mark>" + p + "</mark>,<br> at an interest rate of <mark>" + r + "%</mark> .<br> You will receive an amount of <mark>" + results + "</mark>,<br> in the year <mark>" + future + "</mark> "
 }
 
 function slide() {
